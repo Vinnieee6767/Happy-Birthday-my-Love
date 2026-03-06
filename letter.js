c9d545b558f25564208e42c711e0260f1570e98a
Vinnieee6767 <paulvincentmadlos@gmail.com>
2026-03-06 19:56:27 +0800

    Update letter.js

--- letter.js
+++ letter.js
@@ -45,3 +45,20 @@ function typeLetter() {
 }
 
 typeLetter();
+const audio = document.getElementById("bg-audio");
+
+function playRandomly() {
+    audio.play();
+
+    let randomTime = Math.floor(Math.random() * (60000 - 15000 + 1)) + 15000;
+
+    setTimeout(playRandomly, randomTime);
+}
+
+window.onload = function () {
+    playRandomly();
+};
+
+function goToFun() {
+    window.location.href = "fun.html";
+}
